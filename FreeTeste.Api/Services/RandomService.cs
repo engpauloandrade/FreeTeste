51c71af9df6dd3c2b118f4e82c27f1229c206c2e
Paulo Andrade <81451506+engpauloandrade@users.noreply.github.com>
2023-04-12 00:45:17 -0300

    Asyncsic

--- FreeTeste.Api/Services/RandomService.cs
+++ FreeTeste.Api/Services/RandomService.cs
@@ -7,6 +7,10 @@
         // Caso queira ir mais além, faça um gerador de senhas, utilize a using System.Security.Cryptography;
         // https://learn.microsoft.com/en-us/dotnet/api/system.security.cryptography?view=net-6.0
         // é possível gerar senhas aleatórias com a classe RandomNumberGenerator
+
+        // Aqui poderíamos transformar o método em assincrono? public "task async<int> GenerateRandomNumbersAsync" ? Sim? Não?
+        // Qual é a diferença entre um método síncrono e assíncrono? Qual a necessidade de usar assíncrono?
+        // Sim, você pode pesquisar no chatGPT, é uma ferramenta.
         public int GenerateRandomNumbers()
         {
             return 100;
